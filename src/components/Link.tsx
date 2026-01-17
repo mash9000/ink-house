2f3bdf02b6a357ffc95356a17c3c7be5d60ec4a1
simmetr <vm.certo@gmail.com>
2026-01-17 21:38:28 +0700

    feat: добавлен компонент ссылки

---
+++ src/components/Link.tsx
@@ -0,0 +1,20 @@
+import './link/link.scss';
+import './link/_hover/link--hover.scss';
+import './link/_focus/link--focus.scss';
+import './link/_active/link--active.scss';
+
+import type {FC} from "react";
+
+interface LinkProps {
+    href: URL;
+    title: string;
+}
+
+export const Link: FC<LinkProps> = ({href, title}) => {
+    return (
+        <a
+            className='link link--hover link--focus link--active'
+            href={href.href}
+            target={"_blank"}>{title}</a>
+    );
+}
