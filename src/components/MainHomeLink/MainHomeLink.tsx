01b7f68df89c7cdbad82d9cf6e74d733ec562a8b
simmetr <vm.certo@gmail.com>
2026-01-18 17:30:33 +0700

    feat, style: добавлена главная ссылка на домашнюю страницу

---
+++ src/components/MainHomeLink/MainHomeLink.tsx
@@ -0,0 +1,9 @@
+import './main-link/main-link.scss';
+
+export const MainHomeLink = () => {
+    return (
+        <a className="main-link"
+           href='http://localhost:5173/'
+           target={"_self"}>Ink. House</a>
+    );
+}
