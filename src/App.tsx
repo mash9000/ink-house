6fdcee627efa82508137b6d57bc21990ec13207b
simmetr <vm.certo@gmail.com>
2026-01-18 15:07:49 +0700

    style: товарная карточка стилизована (без адаптива)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import {ProductCard} from "./components/ProductCard/ProductCard.tsx";
+import type {IProduct} from "./components/ProductCard/model/IProduct.ts";
+import {nanoid} from 'nanoid';
+
 export const App = () => {
-  return (
-    <p>check</p>
-  )
+    const product: IProduct = {
+        id: nanoid(),
+        image: {
+            src: '/images/works/france/3.webp',
+            description: 'работа "Процедура"'
+        },
+        ownerOrAuthor: 'Франсуа Дюпон',
+        mainTitle: 'Процедура',
+        technology: 'Цветная литография',
+        size: {
+            width: 40,
+            height: 60
+        },
+        price: {
+            getViewOfCurrency: (value: number) => new Intl.NumberFormat('ru-RU', {
+                style: 'currency',
+                currency: 'RUB',
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+            }).format(value).slice(0, -2) + ' руб'
+        },
+        buttonTitle: 'Купить'
+    }
+
+    return (
+        <ProductCard product={product}>
+            <div className='product-card__description'>
+                <div
+                    className='product-card__description__technology'>{product.technology}</div>
+                <div className='product-card__description__size'>&nbsp;({product.size?.width}x{product.size?.height})</div>
+            </div>
+            <p className='product-card__price'>{product.price.getViewOfCurrency(20000)}</p>
+        </ProductCard>
+    )
 }
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,12 @@
 import './product-card/product-card.scss';
+import './product-card/__image/product-card__image.scss';
+import './product-card/__owner-or-author/product-card__owner-or-author.scss';
+import './product-card/__title/product-card__title.scss';
+import './product-card/__description/product-card__description.scss';
+import './product-card/__description/__technology/product-card__description__technology.scss';
+import './product-card/__description/__size/product-card__description__size.scss';
+import './product-card/__price/product-card__price.scss';
+
 import {Button} from "../Button/Button.tsx";
 import {ButtonForm} from "../Button/model/ButtonForm.ts";
 import type {CSSProperties, FC, ReactNode} from "react";
@@ -21,7 +29,7 @@ export const ProductCard: FC<ProductCardProps> = ({product, children}) => {
                 className="product-card__image"
                 src={product.image.src}
                 alt={product.image.description}/>
-            <p className='product-card__artist'>{product.ownerOrAuthor}</p>
+            <p className='product-card__owner-or-author'>{product.ownerOrAuthor}</p>
             <figcaption className='product-card__title'>{product.mainTitle}</figcaption>
             {children}
             <Button
