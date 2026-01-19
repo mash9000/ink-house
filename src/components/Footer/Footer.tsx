9a0c79f6aef5f9ce0881d5efc0fc771ba0c0353e
simmetr <vm.certo@gmail.com>
2026-01-19 23:18:55 +0700

    feat: footer закончен

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,9 +7,12 @@ import './footer/__blocks/__logo-and-communication/footer__blocks__logo-and-comm
 import './footer/__blocks/__logo-and-communication/__telephone-number/footer__blocks__logo-and-communication__telephone-number.scss';
 import './footer/__blocks/__logo-and-communication/__telephone-number/_number/footer__blocks__logo-and-communication__telephone-number--number.scss';
 import './footer/__blocks/__logo-and-communication/__telephone-number/_subscriber/footer__blocks__logo-and-communication__telephone-number--subscriber.scss';
+import './footer/__blocks/__history/footer__blocks__history.scss';
+import './footer/__blocks/__history/__reproductions/_country/footer__blocks__history__reproductions--country.scss';
 
 import {type FC, type ReactNode} from "react";
 import {MainHomeLink} from "../MainHomeLink/MainHomeLink.tsx";
+import {Link} from "../Link/Link.tsx";
 
 interface FooterProps {
     children?: ReactNode;
@@ -26,7 +29,24 @@ export const Footer: FC<FooterProps> = ({children}) => {
                         <p className='footer__blocks__logo-and-communication__telephone-number--subscriber'>Мастерская</p>
                     </div>
                 </div>
-
+                <div className='footer__blocks__history'>
+                    <div className='footer__blocks__history__reproductions'>
+                        <Link href={new URL('http://localhost:5173/')} title='Репродукции'/>
+                        <p className='footer__blocks__history__reproductions--country'>Франция</p>
+                        <p className='footer__blocks__history__reproductions--country'>Германия</p>
+                        <p className='footer__blocks__history__reproductions--country'>Англия</p>
+                    </div>
+                    <div className='footer__blocks__history__new-items'>
+                        <Link href={new URL('http://localhost:5173/')} title='Новинки'/>
+                        <p className='footer__blocks__history__reproductions--country'>2021</p>
+                        <p className='footer__blocks__history__reproductions--country'>2020</p>
+                    </div>
+                    <div className='footer__blocks__history__about-us'>
+                        <Link href={new URL('http://localhost:5173/')} title='О нас'/>
+                        <p className='footer__blocks__history__reproductions--country'>Художники</p>
+                        <p className='footer__blocks__history__reproductions--country'>Менеджеры</p>
+                    </div>
+                </div>
                 <div className='footer__blocks__social-networks-and-rights'>
                     <div className='footer__blocks__social-networks'>
                         {children}
